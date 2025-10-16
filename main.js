15e946890007f933a1fab485975a996589731faa
Dulloa <dulloa7905@gmail.com>
2025-10-15 22:42:04 -0400

    Add defined style for display canvas

--- main.js
+++ main.js
@@ -5,23 +5,19 @@ console.log("Ping");
 
     //Create scene
     const scene = new THREE.Scene();
-    const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#screen')})
+    const renderer = new THREE.WebGLRenderer({antialias: true,canvas: document.querySelector('#screen')})
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild( renderer.domElement );
     
     //Add camera
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 800);
 
-    //example cube
-    //const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    //const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    //const cube = new THREE.Mesh( geometry, material );
-    //scene.add( cube );
-
     //Create the Earth
-    const earthGeometry = new THREE.SphereGeometry(0.5, 32, 32)
+    const earthGeometry = new THREE.SphereGeometry(1, 35, 35)
     const earthMaterial = new THREE.MeshPhongMaterial({
-        wireframe: true
+        map : new THREE.TextureLoader().load('resources/earth-basic-texture.jpg'),
+        bumpMap : new THREE.TextureLoader().load('resources/earth-elevation-map.jpg'),
+        bumpScale : 0.1,
     })
     const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
 
@@ -37,6 +33,12 @@ console.log("Ping");
     function animate() {
         earthMesh.rotation.x += 0.0001;
         earthMesh.rotation.y += 0.0065;
+        
+
+
+        let canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
         renderer.render( scene, camera );
     }
     renderer.setAnimationLoop( animate );
